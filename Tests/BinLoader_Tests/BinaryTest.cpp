16594aeb9575c1b10de4d0bb34e3e1b26ff146f5
Robbe Bryssinck <robbebryssinck@gmail.com>
2022-04-12 15:13:56 +0200

    BinaryTest fixture

--- Tests/BinLoader_Tests/BinaryTest.cpp
+++ Tests/BinLoader_Tests/BinaryTest.cpp
@@ -1,13 +1,44 @@
 #include <gtest/gtest.h>
 #include <BinLoader/Binary.h>
 #include <BinLoader/BaseParser.h>
+#include <filesystem>
 
 namespace
 {
+  class BinaryTest : public ::testing::Test
+  {
+  public:
+    static void SetUpTestSuite()
+    {
+      // TODO: this directory stuff is bad
+      auto oldPath = std::filesystem::current_path();
+      std::filesystem::current_path(oldPath.parent_path());
+
+      pBinary = Parsing::ParseFile("Samples/test64.exe");
+
+      std::filesystem::current_path(oldPath);
+    }
+
+    static std::shared_ptr<Binary> pBinary;
+  };
+
+  std::shared_ptr<Binary> BinaryTest::pBinary = nullptr;
+
   TEST(Binary, LoadPe64)
   {
+    // TODO: this directory stuff is bad
+    auto oldPath = std::filesystem::current_path();
+    std::filesystem::current_path(oldPath.parent_path());
     std::shared_ptr<Binary> pBinary = Parsing::ParseFile("Samples/test64.exe");
+    std::filesystem::current_path(oldPath);
 
-    EXPECT_NE(pBinary, nullptr);
+    EXPECT_TRUE(pBinary);
+  }
+
+  TEST_F(BinaryTest, GetTextSection)
+  {
+    const Section* pText = pBinary->GetTextSection();
+
+    EXPECT_TRUE(pText);
   }
 }
