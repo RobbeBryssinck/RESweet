1090b6d94d52b9362329c8d6af0babf14d329214
Robbe Bryssinck <robbebryssinck@gmail.com>
2022-04-12 15:47:24 +0200

    elf and dotnet binary parsing tests

--- Tests/BinLoader_Tests/BinaryTest.cpp
+++ Tests/BinLoader_Tests/BinaryTest.cpp
@@ -5,18 +5,25 @@
 
 namespace
 {
+  std::shared_ptr<Binary> ParseFile(const std::string& aFilename)
+  {
+    // TODO: this directory stuff is bad
+    auto oldPath = std::filesystem::current_path();
+    std::filesystem::current_path(oldPath.parent_path());
+
+    auto pBinary = Parsing::ParseFile("Samples/" + aFilename);
+
+    std::filesystem::current_path(oldPath);
+
+    return pBinary;
+  }
+
   class BinaryTest : public ::testing::Test
   {
   public:
     static void SetUpTestSuite()
     {
-      // TODO: this directory stuff is bad
-      auto oldPath = std::filesystem::current_path();
-      std::filesystem::current_path(oldPath.parent_path());
-
-      pBinary = Parsing::ParseFile("Samples/test64.exe");
-
-      std::filesystem::current_path(oldPath);
+      pBinary = ParseFile("test64.exe");
     }
 
     static std::shared_ptr<Binary> pBinary;
@@ -26,11 +33,28 @@ namespace
 
   TEST(Binary, LoadPe64)
   {
-    // TODO: this directory stuff is bad
-    auto oldPath = std::filesystem::current_path();
-    std::filesystem::current_path(oldPath.parent_path());
-    std::shared_ptr<Binary> pBinary = Parsing::ParseFile("Samples/test64.exe");
-    std::filesystem::current_path(oldPath);
+    auto pBinary = ParseFile("test64.exe");
+
+    EXPECT_TRUE(pBinary);
+  }
+
+  TEST(Binary, LoadPe32)
+  {
+    auto pBinary = ParseFile("test.exe");
+
+    EXPECT_TRUE(pBinary);
+  }
+
+  TEST(Binary, LoadPeDotNet64)
+  {
+    auto pBinary = ParseFile("dotnet64test.exe");
+
+    EXPECT_TRUE(pBinary);
+  }
+
+  TEST(Binary, LoadElf64)
+  {
+    auto pBinary = ParseFile("elf64");
 
     EXPECT_TRUE(pBinary);
   }
